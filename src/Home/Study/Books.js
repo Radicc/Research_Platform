1d70a5a96359c2d90d674105db0ec6bb01995c17
Radicc <tomas.radic00@gmail.com>
2023-02-11 20:22:29 +0100

    StudyUpade

---
+++ src/Home/Study/Books.js
@@ -0,0 +1,96 @@
+import "./Study.scss";
+import { useState, useEffect } from "react";
+
+import Babylon from "../images/The_Richest_Man_In_Babylon.jpg";
+import Psychology from "../images/Psychology_of_Money.jpg";
+import OneUpOnWallStreet from "../images/One_Up_On_WallStreet.jpg";
+import RicherWiser from "../images/Richer_Wiser_Happier.jpeg";
+import GoodStocks from "../images/Good_Stocks_Cheap.jpg";
+import IntelligentInvestor from "../images/The_Intelligent_Investor.jpeg";
+
+export default function Books(props) {
+  const booksInfo = [
+    {
+      img: Babylon,
+      name: "Psychology of money",
+      author: "George S. Clason",
+      difficulty: 1,
+    },
+    {
+      img: Psychology,
+      name: "Psychology of money",
+      author: "Morgan Housel",
+      difficulty: 1,
+    },
+    {
+      img: OneUpOnWallStreet,
+      name: "One Up On Wall Street",
+      author: "Peter Lynch",
+      difficulty: 1,
+    },
+    {
+      img: RicherWiser,
+      name: "Richer Wiser Happier",
+      author: "William Green",
+      difficulty: 2,
+    },
+    {
+      img: GoodStocks,
+      name: "Good Stocks Cheap",
+      author: "Kenneth Jeffrey Marshall",
+      difficulty: 2,
+    },
+    {
+      img: IntelligentInvestor,
+      name: "The Intelligent Investor",
+      author: "Benjamin Graham",
+      difficulty: 3,
+    },
+  ];
+  const [bookInfo, setBookInfo] = useState(booksInfo);
+
+  const [filteredArr, setFilteredArr] = useState(0);
+  let content = [];
+
+  const newArr = (x) => {
+    let newX = [];
+    for (let i = 0; i < x.length; i++) {
+      if (x[i].difficulty === props.props) {
+        newX.push(x[i]);
+      }
+    }
+    return newX;
+  };
+
+  const RenderBooks = (y) => {
+    for (let i = 0; i < y.length; i++) {
+      content.push(
+        <div className="BookComponent">
+          <h2>{y[i].name}</h2>
+          <p>by {y[i].author}</p>
+          <img src={y[i].img} />
+        </div>
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (props.props === 1) {
+      const filteredX = newArr(bookInfo);
+      setFilteredArr(filteredX);
+    } else if (props.props === 2) {
+      const filteredX = newArr(bookInfo);
+      setFilteredArr(filteredX);
+    } else {
+      const filteredX = newArr(bookInfo);
+      setFilteredArr(filteredX);
+    }
+  }, [props.props]);
+
+  return (
+    <>
+      {RenderBooks(filteredArr)}
+      {content}
+    </>
+  );
+}
--- src/Home/Study/Study.js
+++ src/Home/Study/Study.js
@@ -1,11 +1,21 @@
 import "./Study.scss";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import Books from "./Books";
 
 export default function Study() {
   const [menuRotation, setMenuRotation] = useState("rotation");
+  const [levelRotation, setLevelRotation] = useState("rotationLevelContainer");
   const [Content, setContent] = useState("contentNone");
+
+  const [propRef, setPropRef] = useState("");
+  const [contentBookFast, setContentBookFast] = useState("BookSection");
+  const contentDefault = "contentNone";
+
   const containerSection = "containerSection selected";
   const containerSectionNone = "containerSectionNone";
+
+  const levelContainer = "levelContainer selected";
+  const levelContainerNone = "levelContainerNone";
 
   const defaultMenu = {
     Books: "containerSection",
@@ -14,66 +24,177 @@ export default function Study() {
     ContentBooks: "contentNone",
     ContentYoutube: "contentNone",
     ContentWebsites: "contentNone",
+
+    Beginner: "levelContainer", // Beginner Button Books
+    BeginnerElements: contentDefault, // Beginner Content
+
+    Intermediate: "levelContainer", // Intermediate Button Books
+    IntermediateElements: contentDefault,
+
+    Advanced: "levelContainer", // Advanced Button Books
+    AdvancedElements: contentDefault,
   };
   const [menuScale, setMenutScale] = useState(defaultMenu);
 
-  const handleClick = (e) => {
+  const handleClickLevel = (e) => {
     const number = e.target.value;
 
-    if (Content != "content1") {
-      setContent("content1");
-    }
+    setPropRef(Number(number));
 
+    if (levelRotation != "rotationLevelContainerRotation") {
+      setLevelRotation("rotationLevelContainerRotation");
+    }
+    if (Content != "content2") {
+      setContent("content2");
+    }
+    if (contentBookFast === "BookSection") {
+      setContentBookFast("BookSectionFast");
+    }
     if (number === "1") {
-      setMenuRotation("rotation2");
       setMenutScale((prev) => ({
         ...prev,
-        Books: containerSection,
-        Youtube: containerSectionNone,
-        Websites: containerSectionNone,
-        ContentBooks: "contentBooks",
-        ContentYoutube: "contentNone",
-        ContentWebsites: "contentNone",
+        Beginner: levelContainer,
+        Intermediate: levelContainerNone,
+        Advanced: levelContainerNone,
+        BeginnerElements: contentBookFast,
+        IntermediateElements: contentDefault,
+        AdvancedElements: contentDefault,
       }));
     } else if (number === "2") {
-      setMenuRotation("rotation2");
       setMenutScale((prev) => ({
         ...prev,
-        Books: containerSectionNone,
-        Youtube: containerSection,
-        Websites: containerSectionNone,
-        ContentBooks: "contentNone",
-        ContentYoutube: "contentYoutube",
-        ContentWebsites: "contentNone",
+        Beginner: levelContainerNone,
+        Intermediate: levelContainer,
+        Advanced: levelContainerNone,
+        BeginnerElements: contentDefault,
+        IntermediateElements: contentBookFast,
+        AdvancedElements: contentDefault,
       }));
     } else {
-      setMenuRotation("rotation2");
       setMenutScale((prev) => ({
         ...prev,
-        Books: containerSectionNone,
-        Youtube: containerSectionNone,
-        Websites: containerSection,
-        ContentBooks: "contentNone",
-        ContentYoutube: "contentNone",
-        ContentWebsites: "contentWebsites",
+        Beginner: levelContainerNone,
+        Intermediate: levelContainerNone,
+        Advanced: levelContainer,
+        BeginnerElements: "contentNone",
+        IntermediateElements: "contentNone",
+        AdvancedElements: contentBookFast,
       }));
     }
   };
+
+  const handleClick1 = () => {
+    if (Content != "content1" && Content != "content2") {
+      setContent("content1");
+    } else {
+      setContent("content1_1");
+    }
+    if (menuScale.Beginner === levelContainer) {
+      setContent("content2");
+    }
+    setMenuRotation("rotation2");
+    setMenutScale((prev) => ({
+      ...prev,
+      Books: containerSection,
+      Youtube: containerSectionNone,
+      Websites: containerSectionNone,
+      ContentBooks: "contentBooks",
+      ContentYoutube: "contentNone",
+      ContentWebsites: "contentNone",
+    }));
+  };
+  const handleClick2 = () => {
+    if (Content != "content1" && Content != "content2") {
+      setContent("content1");
+    }
+    if (
+      menuScale.Books === containerSection ||
+      menuScale.Websites === containerSection
+    ) {
+      setContent("contentFast");
+    }
+
+    setMenuRotation("rotation2");
+    setMenutScale((prev) => ({
+      ...prev,
+      Books: containerSectionNone,
+      Youtube: containerSection,
+      Websites: containerSectionNone,
+      ContentBooks: "contentNone",
+      ContentYoutube: "contentYoutube",
+      ContentWebsites: "contentNone",
+    }));
+  };
+  const handleClick3 = () => {
+    if (Content != "content1" && Content != "content2") {
+      setContent("content1");
+    }
+    if (
+      menuScale.Books === containerSection ||
+      menuScale.Youtube === containerSection
+    ) {
+      setContent("contentFast");
+    }
+    setMenuRotation("rotation2");
+    setMenutScale((prev) => ({
+      ...prev,
+      Books: containerSectionNone,
+      Youtube: containerSectionNone,
+      Websites: containerSection,
+      ContentBooks: "contentNone",
+      ContentYoutube: "contentNone",
+      ContentWebsites: "contentWebsites",
+    }));
+  };
+
   return (
     <div className="Study">
       <div className={menuRotation}>
-        <button value={1} onClick={handleClick} className={menuScale.Books}>
+        <button value="1" onClick={handleClick1} className={menuScale.Books}>
           Books
         </button>
-        <button value="2" onClick={handleClick} className={menuScale.Youtube}>
+        <button value="2" onClick={handleClick2} className={menuScale.Youtube}>
           Youtube Channels
         </button>
-        <button value="3" onClick={handleClick} className={menuScale.Websites}>
+        <button value="3" onClick={handleClick3} className={menuScale.Websites}>
           Websites
         </button>
       </div>
       <div className={Content}>
-        <div className={menuScale.ContentBooks}>Books</div>
+        <div className={menuScale.ContentBooks}>
+          <div className={levelRotation}>
+            <button
+              value="1"
+              onClick={handleClickLevel}
+              className={menuScale.Beginner}
+            >
+              Beginner
+            </button>
+            <button
+              value="2"
+              onClick={handleClickLevel}
+              className={menuScale.Intermediate}
+            >
+              Intermediate
+            </button>
+            <button
+              value="3"
+              onClick={handleClickLevel}
+              className={menuScale.Advanced}
+            >
+              Advanced
+            </button>
+          </div>
+          <div className={menuScale.BeginnerElements}>
+            <Books props={propRef} />
+          </div>
+          <div className={menuScale.IntermediateElements}>
+            <Books props={propRef} />
+          </div>
+          <div className={menuScale.AdvancedElements}>
+            <Books props={propRef} />
+          </div>
+        </div>
         <div className={menuScale.ContentYoutube}>Youtube</div>
         <div className={menuScale.ContentWebsites}>Websites</div>
       </div>
