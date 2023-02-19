8ed9700f77ae5351b78d8147e59433fea94adbfb
Radicc <tomas.radic00@gmail.com>
2023-02-19 19:34:15 +0100

    update

--- src/Home/FirstSection.js
+++ src/Home/FirstSection.js
@@ -1,11 +1,24 @@
+import { NavLink } from "react-router-dom";
 import "./sass/_firstSection.scss";
-import wallstreet_1 from "./images/wallstreet_1.jpg";
+import wallstreet_3 from "./images/wallstreet_3.jpg";
+import study from "./images/StudySection.jpg";
+import calculatorOldMan from "./images/RetirementCalculator12.jpg";
 
 export default function FirstSection() {
   return (
     <div className="firstSection">
       <div className="titulContainer">
         <h1>SUCCESS IN ANY FIELD OF LIFE IS NOT DUE TO INBORN TRAITS</h1>
+      </div>
+      <div className="featureContainer">
+        <NavLink to="/Study" className="contentFeatureContainer">
+          <h1>Study</h1>
+          <img src={study} />
+        </NavLink>
+        <NavLink to="/RetirmentCalculator" className="contentFeatureContainer">
+          <h1>Retirement Calculator</h1>
+          <img src={calculatorOldMan} />
+        </NavLink>
       </div>
     </div>
   );
