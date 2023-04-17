58c218c6fbf7add52472e020c86168190014cd6a
Radicc <tomas.radic00@gmail.com>
2023-04-17 12:53:10 +0200

    Improving UI

--- src/Home/RetirmentCalculator/Results.tsx
+++ src/Home/RetirmentCalculator/Results.tsx
@@ -17,27 +17,52 @@ const Results = ({ defaultForm }: Props) => {
 
       <div className="containerSolution">
         <h2>Saved</h2>
-        <p>${defaultForm.NormalSaveCal?.toFixed(0) || 0}</p>
+        <p>
+          {defaultForm.NormalSaveCal?.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          }) || 0}
+        </p>
       </div>
 
       <div className="containerSolution">
         <h2>Growth</h2>
-        <p>${defaultForm.GrowthCal?.toFixed(0) || 0}</p>
+        <p>
+          {defaultForm.GrowthCal?.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          }) || 0}
+        </p>
       </div>
 
       <div className="containerSolution">
         <h2>Balance ( {defaultForm.RetirementAge || "?"} yo )</h2>
-        <p>${defaultForm.LastYearCal?.toFixed(0) || 0}</p>
+        <p>
+          {defaultForm.LastYearCal?.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          }) || 0}
+        </p>
       </div>
 
       <div className="containerSolution">
-        <h2>RetirementIncome</h2>
-        <p>${defaultForm.RetirementIncome?.toFixed(0) || 0}</p>
+        <h2>Retirement Income</h2>
+        <p>
+          {defaultForm.RetirementIncome?.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          }) || 0}
+        </p>
       </div>
 
       <div className="containerSolution">
         <h2>BalanceEnd ( {defaultForm.ExpectToLive || "?"} yo )</h2>
-        <p>${defaultForm.BalancedEnd?.toFixed(0) || 0}</p>
+        <p>
+          {defaultForm.BalancedEnd?.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          }) || 0}
+        </p>
       </div>
     </>
   );
--- src/Home/Stocks/StocksSkeletonGrid.tsx
+++ src/Home/Stocks/StocksSkeletonGrid.tsx
@@ -6,15 +6,13 @@ const StocksSkeletonGrid = () => {
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
   return (
-    <div className="Stocks">
-      <div className="containerSectionStocks">
-        {skeletons.map((item) => (
-          <div key={item} className="containerStocks">
-            <Spinner animation="border" variant="warning" />
-          </div>
-        ))}
-      </div>
-    </div>
+    <>
+      {skeletons.map((item) => (
+        <div key={item} className="containerStocks">
+          <Spinner animation="border" variant="warning" />
+        </div>
+      ))}
+    </>
   );
 };
 
