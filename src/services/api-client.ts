48bdbde964e2a43b920c93a2097232bbca841277
Radicc <tomas.radic00@gmail.com>
2023-05-02 13:53:55 +0200

    newKEY

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -5,6 +5,6 @@ export default axios.create({
   baseURL: "https://apidojo-yahoo-finance-v1.p.rapidapi.com/news",
   headers: {
     "X-RapidAPI-Key": "ec93c127acmsh87a505023594f89p1f9a2bjsn5dd21ed35501",
-    "X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com",
+    "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
   },
 });
