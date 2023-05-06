6121dce48b5a330a8e8da32cdfc713dfcf736091
Radicc <tomas.radic00@gmail.com>
2023-05-06 22:40:59 +0200

    StocksAnalyzer

--- src/Home/StocksAnalyzer/StocksAnalyzer.tsx
+++ src/Home/StocksAnalyzer/StocksAnalyzer.tsx
@@ -5,6 +5,33 @@ const StocksAnalyzer = () => {
   return (
     <div className="StocksAnalyzer">
       <h1>StocksAnalyzer</h1>
+      <input placeholder="Search for stocks" />
+      <div className="container">
+        <div className="data">
+          <h1>Years</h1>
+        </div>
+        <div className="data">
+          <h1>Revenue growth</h1>
+        </div>
+        <div className="data">
+          <h1>Net Income</h1>
+        </div>
+        <div className="data">
+          <h1>Total Assets</h1>
+        </div>
+        <div className="data">
+          <h1>Total Liabilities</h1>
+        </div>
+        <div className="data">
+          <h1>Total Equity</h1>
+        </div>
+        <div className="data">
+          <h1>Free Cash FLow</h1>
+        </div>
+        <div className="data">
+          <h1>Shares Outstanding</h1>
+        </div>
+      </div>
     </div>
   );
 };
